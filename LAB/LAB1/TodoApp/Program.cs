b7bcea49a3c46d00e5a0a85053c895836a4396ce
Nguyễn Quốc Huy <162018744+Beakjoen@users.noreply.github.com>
2026-05-10 16:38:51 +0700

    Push folder LAB

---
+++ LAB/LAB1/TodoApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Text;
+using TodoApp;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+        Console.InputEncoding = Encoding.UTF8;
+        new TodoUI().Run();
+    }
+}
